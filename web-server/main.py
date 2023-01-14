e0546721a1f2d67328a8197e3875d5f8c2e0f6ac
ryes <roger.reyesm97@gmail.com>
2023-01-13 23:20:31 -0500

    fastapi

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,19 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get("/", response_class=HTMLResponse)
+def get_list():
+    return """
+    <h1>Hello I'm Roger Reys</h1>
+    <h2>Welcome</h2>
+    """
+
+@app.get("/contact")
+def get_list():
+    return {'name': 'roger_reys'}
 
 def run():
     store.get_catalog()
