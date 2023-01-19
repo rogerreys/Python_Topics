74c99a080c108de74d4c119f442c9170528ef6a8
ryes <roger.reyesm97@gmail.com>
2023-01-18 23:01:26 -0500

    Algoritmo

---
+++ algoritmos/factorial.py
@@ -0,0 +1,30 @@
+import time
+
+def factorial(n):
+    res = 1 
+
+    while n>1:
+        res *= n
+        n-=1
+    return res
+
+def factorial_r(n):
+    if n==1: 
+        return 1
+    return n * factorial(n-1)
+
+def run():
+    n = 500000
+    comienzo = time.time()
+    factorial(n)
+    final = time.time()
+    print("Esc1: ",final - comienzo)
+    
+    comienzo = time.time()
+    factorial_r(n)
+    final = time.time()
+    print("Esc2: ",final - comienzo)
+
+
+if __name__=='__main__':
+    run()
